6f85fc958f9cefe2fbab9722efc6d6a57b366057
He Gu Yi <ezcommerce@outlook.com>
2012-08-28 21:32:08 +0800

    Translated comments into English

--- includes/classes/captcha.php
+++ includes/classes/captcha.php
@@ -5,41 +5,38 @@
 define('CPT', 'cpt');
 
 class captcha{
-	//图片对象、宽度、高度、验证码长度
+	// Image object, width, height and the lenght of the captcha
 	private $im;
 	private $im_width;
 	private $im_height;
 	private $len;
-	//随机字符串、y轴坐标值、随机颜色
+	// Random number, y axis and random color
 	private $randnum;
 	private $y;
 	private $randcolor;
-	//背景色的红绿蓝，默认是浅灰色
+	// Background red/grean/blue color，default color is light gray
 	public $red = 238;
 	public $green = 238;
 	public $blue = 238;
-	/**
-	 * 可选设置：验证码类型、干扰点、干扰线、Y轴随机
-	 * 设为 false 表示不启用
-	 **/
-	//默认是大小写数字混合型，1 2 3 分别表示 小写、大写、数字型
+	
+	// Number+letters by default
+	// 1 2 3 represent lower case, upper case and numbers only
 	public $ext_num_type='';
-	public $ext_pixel = false; //干扰点
-	public $ext_line = false; //干扰线
-	public $ext_rand_y = true; //Y轴随机
+	public $ext_pixel = false; // Disturbance spots
+	public $ext_line = false; // Disturbance lines
+	public $ext_rand_y = true; // Random y axis
 	
 	function __construct($len = 4, $im_width = '', $im_height = 25){
-		// 验证码长度、图片宽度、高度是实例化类时必需的数据
 		$this->len = $len; $im_width = $len * 15;
 		$this->im_width = $im_width;
 		$this->im_height= $im_height;
 		$this->im = imagecreate($im_width, $im_height);
 	}
-	// 设置图片背景颜色，默认是浅灰色背景
+	// Set the background color, light gray by default
 	function set_bgcolor(){
 		imagecolorallocate($this->im,$this->red,$this->green,$this->blue);
 	}
-	// 获得任意位数的随机码
+	// Get a random number
 	function get_randnum(){
 		$an1 = 'abcdefghijklmnopqrstuvwxyz';
 		$an2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -52,21 +49,21 @@ class captcha{
 			$start = rand(1,strlen($str) - 1);
 			$randnum .= substr($str,$start,1);
 		}
-		$this->randnum = $randnum;
+		$this->randnum = strtolower($randnum); // Case insensitive
 		$_SESSION[CPT] = $this->randnum;
 	}
-	// 获得验证码图片Y轴
+	// Get the y axis of the captcha
 	function get_y(){
 		if($this->ext_rand_y)
 			$this->y = rand(5, $this->im_height / 5);
 		else
 			$this->y = $this->im_height / 4 ;
 	}
-	// 获得随机色
+	// Get a random number
 	function get_randcolor() {
 		$this->randcolor = imagecolorallocate($this->im,rand(0,100),rand(0,150),rand(0,200));
 	}
-	// 添加干扰点
+	// Add disturbance spots
 	function set_ext_pixel(){
 		if($this->ext_pixel){
 			for($i = 0; $i < 100; $i++){
@@ -75,7 +72,7 @@ class captcha{
 			}
 		}
 	}
-	// 添加干扰线
+	// Add disturbance lines
 	function set_ext_line(){
 		if($this->ext_line){
 			for($j = 0; $j < 2; $j++){
@@ -88,14 +85,7 @@ class captcha{
 			}
 		}
 	}
-	/**创建验证码图像：
-	 * 建立画布（__construct函数）
-	* 设置画布背景（$this->set_bgcolor();）
-	* 获取随机字符串（$this->get_randnum ();）
-	* 文字写到图片上（imagestring函数）
-	* 添加干扰点/线（$this->set_ext_line(); $this->set_ext_pixel();）
-	* 输出图片
-	**/
+
 	function create(){
 		$this->set_bgcolor();
 		$this->get_randnum();
@@ -110,7 +100,7 @@ class captcha{
 		$this->set_ext_pixel();
 		header("content-type:image/png");
 		imagepng($this->im);
-		imagedestroy($this->im); //释放图像资源
+		imagedestroy($this->im); // Release the image resource
 	}
 }
 ?>
