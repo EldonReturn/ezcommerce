e6549796875ec32ec7ce602117ff7858ea6824e1
He Gu Yi <ezcommerce@outlook.com>
2012-09-20 21:30:16 +0800

    Translate the create_account email content

--- includes/languages/chinese/create_account.php
+++ includes/languages/chinese/create_account.php
@@ -22,7 +22,7 @@ define('EMAIL_GREET_MR', '尊敬的 %s 先生,' . "\n\n");
 define('EMAIL_GREET_MS', '尊敬的 %s 女士' . "\n\n");
 define('EMAIL_GREET_NONE', '尊敬的 %s' . "\n\n");
 define('EMAIL_WELCOME', '恭喜您成为 <strong>' . STORE_NAME . '</strong> 会员 ！' . "\n\n");
-define('EMAIL_TEXT', 'You can now take part in the <strong>various services</strong> we have to offer you. Some of these services include:' . "\n\n" . '<li><strong>Permanent Cart</strong> - Any products added to your online cart remain there until you remove them, or check them out.' . "\n" . '<li><strong>Address Book</strong> - We can now deliver your products to another address other than yours! This is perfect to send birthday gifts direct to the birthday-person themselves.' . "\n" . '<li><strong>Order History</strong> - View your history of purchases that you have made with us.' . "\n" . '<li><strong>Products Reviews</strong> - Share your opinions on products with our other customers.' . "\n\n");
+define('EMAIL_TEXT', '我们为您提供了<strong>多种多样的服务</strong>。其中包括：' . "\n\n" . '<li><strong>永久性的购物车</strong> —— 所有您在线购物车中的商品会永久保留，直到您主动删除或者付款。' . "\n" . '<li><strong>地址薄</strong> —— 您也可以将所购商品快递至您联系地址以外的地方！这在给别人送礼物时非常有用。礼物能直接送到对方手中。' . "\n" . '<li><strong>订购历史</strong> —— 查看您在我们网站购买商品的历史订单。' . "\n" . '<li><strong>商品评价</strong> —— 和我们的其他顾客分享您对商品的意见和建议。' . "\n\n");
 define('EMAIL_CONTACT', '如果您对我们的服务有任何意见或建议, 请联系我们: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
 define('EMAIL_WARNING', '<strong>提示:</strong> 这封邮件是发给我们的一位注册会员. 如您并没有注册, 请联系我们' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n");
 ?>
